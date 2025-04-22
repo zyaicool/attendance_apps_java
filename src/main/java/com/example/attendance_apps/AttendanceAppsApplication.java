6798ebbcf97948d52ef455e5fe9d84500aab3b62
Muhammad Syaichul Islam <zyaicooldragneel@gmail.com>
2025-04-22 13:31:44 +0700

    initiate project

---
+++ src/main/java/com/example/attendance_apps/AttendanceAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.attendance_apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/attendance_apps/AttendanceAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendance_apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
